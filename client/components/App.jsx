b21cf555182c55c6fc6d2c7bab6eb9e3f5342052
Grace Lindelien <gracelindelien@gmail.com>
2020-09-12 14:26:27 -0700

    Create CSS file, add icon to public dir

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,7 +4,13 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   render() {
     return (
-      <h1>Fiesta Party!</h1>
+      <div className="main">
+        <div>
+          <h1>Fiesta Party!</h1>
+        </div>
+        <div className="input"></div>
+        <div className="progress"></div>
+      </div>
     );
   }
 }
