8463ab4db963c38ff7434e767db25aed4368f3e9
Grace Lindelien <gracelindelien@gmail.com>
2020-09-12 12:47:48 -0700

    Setup webpack, babel, render React to page

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.jsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+};
