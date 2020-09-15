12ed24543723ffa89aa4019b9973574b1f3ce88d
Grace Lindelien <gracelindelien@gmail.com>
2020-09-14 19:51:16 -0700

    Move Level 1 content into new module

---
+++ client/levels.js
@@ -0,0 +1,34 @@
+const Levels = [
+
+  {
+    englishWords: [
+      'party',
+      'hello',
+      'goodbye',
+      'dog',
+      'cat',
+      'food',
+      'taco',
+      'beer',
+      'music',
+      'code'
+    ],
+
+    spanishWords: {
+      party: ['fiesta'],
+      hello: ['hola'],
+      goodbye: ['adios'],
+      dog: ['perro', 'perra'],
+      cat: ['gato', 'gata'],
+      food: ['comida'],
+      taco: ['taco'],
+      beer: ['cerveza'],
+      music: ['musica'],
+      code: ['codigo']
+    }
+
+  }
+
+];
+
+export default Levels;
