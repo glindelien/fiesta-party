f868ef133263d7b6bdfe9b2b5353be59486d5138
Grace Lindelien <gracelindelien@gmail.com>
2020-10-15 00:03:41 -0700

    Add Babel config file - required for parsing Jest tests

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+// Babel config
+
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    [
+      '@babel/preset-react',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+};
